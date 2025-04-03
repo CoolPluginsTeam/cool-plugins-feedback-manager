e5a9139e2bcc104e4ecb8573233a754d44e10f69
jyoti <jyoticoolplugins@gmail.com>
2025-04-03 15:13:52 +0530

    Removed extra code

--- feedback/js/admin-feedback.js
+++ feedback/js/admin-feedback.js
@@ -97,68 +97,6 @@
             window.location = plugin_deactivate_link;
         });
 
-        $(document).on("click", ".more-details-link", function (event) {
-            console.log('hello');
-
-            event.preventDefault();
-        
-            var itemId = $(this).data("id");
-            
-            if ($("#popup-box").length === 0) {
-
-                $("body").append(`<div id="popup-box" class="popup-container"> <div class="popup-content">
-                <div class="cpfm-loader"></div>
-                </div> <button id="close-popup">Close</button></div>`);
-
-            } else {
-
-                $("#popup-box").show();
-            }
-
-            let defaultSelectedValue = $("#popup-select").val();
-            sendAjaxRequest(defaultSelectedValue, itemId);
-        });     
-        
-        $(document).on("change", "#popup-select", function (event) {
-          
-            event.preventDefault();
-            var itemId = $(this).data("id");
-          
-            sendAjaxRequest(selectedValue, itemId);
-            
-        });
-
-        function sendAjaxRequest(selectedValue, itemId) {
-
-            $.ajax({
-                url: ajaxurl,
-                type: "POST",
-                data: {
-                    action: "cpfm_get_extra_data",
-                    value: selectedValue,
-                    item_id: itemId,
-                    nonce: ajax_object.nonce,
-                },
-                success: function (response) {
-
-                    let data = JSON.parse(response);
-                    $('#table-container').html('');
-                    $(".popup-content").html(data.html);
-                  
-                },
-                error: function (error) {
-                    console.log("AJAX Error:", error);
-                },
-            });
-        }
-        
-        $(document).on('click', function(event) {
-
-            if($(event.target).is('#close-popup') || !$(event.target).closest('#popup-box, .more-details-link').length){
-                $('#popup-box').fadeOut(300, function () {
-                    $(this).remove();
-                });
-            }            
-        });
+     
     });
 })(jQuery);
